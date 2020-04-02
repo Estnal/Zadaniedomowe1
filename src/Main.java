9dd35edea5b956268690eb5ab169a6c78e99ab9b
Damian Rogoz <d.rogoz@o2.pl>
2020-04-02 23:12:52 +0200

    Kolejna poprawka 1

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+        System.out.println("Hello World");
+        System.out.println("Hello World123");
     }
 }
