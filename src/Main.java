af2e610cf80d7fa7e7a63c1d76152361cde0e333
Damian Rogoz <d.rogoz@o2.pl>
2020-04-02 22:50:26 +0200

    1 dodanie plików Uczę się programowania

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+
+
+    }
+
+
+
+}
